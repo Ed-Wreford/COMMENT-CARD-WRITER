b507fa51c7591e4b9b22a514b859be56d190af6d
Ed-Wreford <wreford.e@etoncollege.org.uk>
2023-02-07 11:18:03 +0000

    Made a second view for which I need to add the comment

---
+++ CommentCardWriter/View/CommentCardView.swift
@@ -0,0 +1,20 @@
+//
+//  CommentCardView.swift
+//  CommentCardWriter
+//
+//  Created by Wreford, Edward (PGW) on 07/02/2023.
+//
+
+import SwiftUI
+
+struct CommentCardView: View {
+    var body: some View {
+        Text("Test")
+    }
+}
+
+struct CommentCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CommentCardView()
+    }
+}
