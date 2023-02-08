d09e672d9bb30330907493169f1bfd58297e3f90
Ed-Wreford <wreford.e@etoncollege.org.uk>
2023-02-08 09:41:44 +0000

    Started making the class

---
+++ CommentCardWriter/Model/Division.swift
@@ -0,0 +1,14 @@
+//
+//  Division.swift
+//  CommentCardWriter
+//
+//  Created by Wreford, Edward (PGW) on 08/02/2023.
+//
+
+import Foundation
+
+class Students {
+    
+    var name: String
+    
+}
